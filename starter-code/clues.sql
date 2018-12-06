b6558c2cfe9b9b06629615f75b7ac268e38c2ce3
Jackie Casper <casper.jaclyn@gmail.com>
2018-12-06 17:03:29 +0300

    solutions

--- starter-code/clues.sql
+++ starter-code/clues.sql
@@ -1,39 +1,40 @@
 -- Clue #1: We recently got word that someone fitting Carmen Sandiego's description has been
 -- traveling through Southern Europe. She's most likely traveling someplace where she won't be noticed,
 -- so find the least populated country in Southern Europe, and we'll start looking for her there.
-
+SELECT * FROM country WHERE region = 'Southern Europe' ORDER BY population ASC LIMIT 1;
 
 
 
 -- Clue #2: Now that we're here, we have insight that Carmen was seen attending language classes in
 -- this country's officially recognized language. Check our databases and find out what language is
 -- spoken in this country, so we can call in a translator to work with you.
-
+SELECT countrycode, language FROM country, countrylanguage WHERE region = 'Southern Europe' AND countrylanguage.countrycode = country.code ORDER BY country.population ASC LIMIT 1;
+SELECT * FROM countrylanguage WHERE countrycode = 'VAT';
 
 
 -- Clue #3: We have new news on the classes Carmen attended – our gumshoes tell us she's moved on
 -- to a different country, a country where people speak only the language she was learning. Find out which
 --  nearby country speaks nothing but that language.
-
+SELECT country.name FROM countrylanguage, country WHERE countrylanguage.language = 'Italian' AND countrylanguage.countrycode = country.code AND countrylanguage.percentage = 100;
 
 
 -- Clue #4: We're booking the first flight out – maybe we've actually got a chance to catch her this time.
  -- There are only two cities she could be flying to in the country. One is named the same as the country – that
  -- would be too obvious. We're following our gut on this one; find out what other city in that country she might
  --  be flying to.
-
+SELECT city.name FROM city, country WHERE country.code = city.countrycode AND country.name= 'San Marino' AND city.name != 'San Marino';
 
 
 -- Clue #5: Oh no, she pulled a switch – there are two cities with very similar names, but in totally different
 -- parts of the globe! She's headed to South America as we speak; go find a city whose name is like the one we were
 -- headed to, but doesn't end the same. Find out the city, and do another search for what country it's in. Hurry!
-
+SELECT city.name, country.name FROM city, country WHERE country.continent = 'South America' AND city.name LIKE('Serra%') AND city.countrycode = country.code;
 
 
 -- Clue #6: We're close! Our South American agent says she just got a taxi at the airport, and is headed towards
  -- the capital! Look up the country's capital, and get there pronto! Send us the name of where you're headed and we'll
  -- follow right behind you!
-
+SELECT city.name FROM city, country WHERE country.name = 'Brazil' AND city.id = country.capital;
 
 
 -- Clue #7: She knows we're on to her – her taxi dropped her off at the international airport, and she beat us to
@@ -49,7 +50,7 @@
 -- I need a little more sunshine with my slice of life.
 -- So I'm off to add one to the population I find
 -- In a city of ninety-one thousand and now, eighty five.
-
+SELECT name FROM city where population = 91084;
 
 -- We're counting on you, gumshoe. Find out where she's headed, send us the info, and we'll be sure to meet her at the gates with bells on.
 
